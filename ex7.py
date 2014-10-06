8d476aec6ca46b5c5608f7f795f9603840d1d952
Ian Edwards <iiSwervy@gmail.com>
2014-10-06 14:41:30 -0700

    Exercise 7 Study Drill 1.

--- ex7.py
+++ ex7.py
@@ -19,3 +19,25 @@ end12 = "r"
 # watch that comma at the end. try removing it to see what happens
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
+
+# Line 1 is meant to say a specific string of code.
+# Line 2 is meant to say a specific string of code and has an included variable.
+# Line 3 is meant to say a specific string of code.
+# Line 4 created a border that is 10 spaces long, made of periods.
+# Line 5 does nothing.
+# Line 6 is meant to say C.
+# Line 7 is meant to say H.
+# Line 8 is meant to say E.
+# Line 9 is meant to say E.
+# Line 10 is meant to say S.
+# Line 11 is meant to say E.
+# Line 12 is meant to say B.
+# Line 13 is meant to say U.
+# Line 14 is meant to say R.
+# Line 15 is meant to say G.
+# Line 16 is meant to say E.
+# Line 17 is meant to say R.
+# Line 18 does nothing.
+# Line 19 has a comment.
+# Line 20 is meant to add end 1, 2, 3, 4, 5, and 6.
+# Line 21 is meant to add end 6, 7, 8, 9, 10, 11, and 12.
