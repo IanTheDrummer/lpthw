dbffe8580a9ca2b0451fa9ead823365d9cb2ab86
Ian Edwards <iiSwervy@gmail.com>
2014-11-17 17:40:13 -0800

    Exercise 19 Study Drill 1 Response

--- ex19.py
+++ ex19.py
@@ -22,3 +22,29 @@ cheese_and_crackers(10 = 20, 5 + 6)
 
 print "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese = 100, amount_of_cheese = 1000)
+
+# Line 1 defines a function.
+# Line 2 prints text and includes a variable.
+# Line 3 prints text and includes a variable.
+# Line 4 prints text.
+# Line 5 prints text and has a linefeed.
+# Line 6 does nothing.
+# Line 7 does nothing.
+# Line 8 prints text.
+# Line 9 gives cheese_and_crackers a value.
+# Line 10 does nothing.
+# Line 11 does nothing.
+# Line 12 prints text.
+# Line 13 gives amount_of_cheese a value.
+# Line 14 gives amount_of_crackers a value.
+# Line 15 does nothing.
+# I'm not sure as to what line 16 does.
+# Line 17 does nothing.
+# Line 18 does nothing.
+# Line 19 prints text.
+# Line 20 does math.
+# Line 21 does nothing.
+# Line 22 does nothing.
+# Line 23 prints text.
+# Line 24 gives things values.
+# Line 25 does nothing.
