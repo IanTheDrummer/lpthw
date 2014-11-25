5fa4146e902d8079b9ed1eaa88ab00029a2c23a0
Ian Edwards <iiSwervy@gmail.com>
2014-11-25 09:54:00 -0800

    Exercise 22 file and paragraph

---
+++ ex22.py
@@ -0,0 +1,27 @@
+Symbols:
++ plus = used in addition
+- minus = used in subtraction
+/ forward slash = generally has code after it that effects more code after the /
+\ back slash = generally has code before it that effects more code after the \
+* asterisk = used for multiplication
+% percent = used with variables
+< less than = showed that something is less than something else
+> greater than = showed that something is greater than something else
+<= less than equal = showed that something is less than equal
+=> greater than equal = showed that something is greater than equal
+" double quotes = used to create strings and hold text
+' single quotes = used to create strings and hold text
+() parenthesis = generally contains something
+: colon = generally used with functions or showing a part of something
+? question mark = used for showing that something could have a possible answer
+= equal = tells that things have similar value
+# hashtag = used to make comments
+
+Words:
+def = define
+print = prints text
+return = returns something to it's original value
+import = imports user specified file
+pass = used in an argument
+else = changes code if something specifically happens
+
