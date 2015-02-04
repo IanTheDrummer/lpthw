982af6377711b3ada4318609c13c44582e4691c7
eykd <david.eyk@gmail.com>
2015-02-03 23:19:59 -0800

    Fix remaining issues w/ ex26.py
    Final score: 17/37
    
    We'll work on that.

--- ex26.py
+++ ex26.py
@@ -7,7 +7,7 @@ def sort_words(words):
     """Sorts the words."""
     return sorted(words)
 
-def print_first_word(words)
+def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
     print word
@@ -20,7 +20,7 @@ def print_last_word(words):
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
-    return sorted_words(words)
+    return sort_words(words)
 
 def print_first_and_last(sentence):
     """Prints the first and last words of the sentence."""
@@ -43,7 +43,7 @@ poem = """
 with logic so firmly planted
 cannot discern \n the needs of love
 nor comprehend passion from intuition
-and requires an explantion
+and requires an explanation
 \n\t\twhere there is none.
 """
 
@@ -63,7 +63,7 @@ def secret_formula(started):
 
 
 start_point = 10000
-beans, jars, crates == secret_formula(start-point)
+beans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
 print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
@@ -71,21 +71,21 @@ print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont
+print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
 
 
-sentence = "All good\tthings come to those who weight."
+sentence = "All good\tthings come to those who wait."
 
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
+words = break_words(sentence)
+sorted_words = sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
 print_first_word(sorted_words)
 print_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
+sorted_words = sort_sentence(sentence)
 print sorted_words
 
 print_first_and_last(sentence)
 
-   print_first_and_last_sorted(sentence)
+print_first_and_last_sorted(sentence)
