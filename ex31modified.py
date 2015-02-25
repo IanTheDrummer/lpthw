31e8b4abd3b95cab285155f29a3873b52332662a
Ian Edwards <iiSwervy@gmail.com>
2015-02-25 12:54:53 -0800

    Exercise 31 Study Drill #1
    Here's an edited version of ex31.py
    I had to add some more additions onto the code... so I did!
    I added a third door. I strongly suggest that when you run it, you
    should go through door 3.
    - Ian Edwards

---
+++ ex31modified.py
@@ -0,0 +1,45 @@
+print "You enter a dark room with three doors. Do you go through door #1 or #2 or #3?"
+
+door = raw_input("> ")
+
+if door == "1":
+	print "There's a giant bear here eating a cheese cake. What do you do?"
+	print "1. Take the cake."
+	print "2. Scream at the bear."
+	
+	bear = raw_input("> ")
+
+	if bear == "1":
+		print "The bear eats your face off. Good Job!"
+	elif bear == "2":
+		print "The bear eats your legs off. Good Job!"
+	else:
+		print "Well, doing %s is probably better. Bear runs away." % bear
+	
+if door == "2":
+	print "You stare into the endless abyss at Cthulhu's retina."
+	print "1. Blueberries."
+	print "2. Yellow Jacket clothespins."
+	print "3. Understanding revolvers yelling melodies."
+	
+	insanity = raw_input("> ")
+	
+	if insanity == "1" or insantiy == "2":
+		print "Your body survives powered by a mind of jello. Good job!"
+	else:
+		print "The insanity rots your eyes into a pool of muck. Good job!"
+		
+if door == "3":
+	print "You've found 100,000,000 dollars! What will you spend it on?"
+	print "1. A Ferrari."
+	print "2. A Banana."
+	print "3. Funding Iona Community."
+	
+	money = raw_input("> ")
+	
+	if money == "1":
+			print "You end up wrecking the car in the dealership parking lot. Good Job!"
+	if money == "2":
+			print "A nearby bear finds you and steals you banana. Good Job!"
+	if money == "3":
+			print "The money turns Iona into a million dollar company and they get robbed causing Father Bob to join Al Queda! Good Job!"
